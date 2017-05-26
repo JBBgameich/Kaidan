bc7cfcf7439204ef81a00e17543e02f16e3ca2a0
JBBgameich <jbbgameich@outlook.com>
2017-05-26 12:53:44 +0200

    Use newer pbuilder version

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-sudo apt install pbuilder ca-certificates git-core tar gzip devscripts lintian;
+sudo apt install ca-certificates git-core tar gzip devscripts lintian debhelper;
+
+mkdir ../cache; cd ../cache;
+wget http://ftp.de.debian.org/debian/pool/main/p/pbuilder/pbuilder_0.228.7_all.deb;
+sudo dpkg -i pbuilder_0.228.7_all.deb;
+sudo apt install -f -y;
+cd ../; rm -r cache;
 
 echo "Finished installing packages for the pbuilder build on trvis-ci host"
