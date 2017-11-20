76ce86cf9f6f79885ab43b7d8cf9140a29f6167c
JBBgameich <jbbgameich@gmail.com>
2017-11-20 18:23:16 +0100

    Add hack to enable appimages in docker

--- utils/AppImage.sh
+++ utils/AppImage.sh
@@ -7,8 +7,9 @@ echo "*****************************************"
 # Download linuxdeployqt
 if ! [ -f linuxdeployqt-continuous-x86_64.AppImage ]; then
     wget https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
-    chmod +x linuxdeployqt-continuous-x86_64.AppImage
 fi
+
+./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract
 
 # configure buildsystem and environment for AppDir installation
 mkdir ../build
@@ -22,7 +23,7 @@ make install
 cd ..
 
 # Copy dependencies into AppDir
-./utils/linuxdeployqt-continuous-x86_64.AppImage $PWD/AppDir/usr/share/applications/kaidan.desktop -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/ -bundle-non-qt-libs
+./utils/squashfs-root/AppRun $PWD/AppDir/usr/share/applications/kaidan.desktop -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml/ -bundle-non-qt-libs
 
 # Create AppImage
-./utils/linuxdeployqt-continuous-x86_64.AppImage $PWD/AppDir/usr/share/applications/kaidan.desktop -appimage
+./utils/squashfs-root/AppRun $PWD/AppDir/usr/share/applications/kaidan.desktop -appimage
