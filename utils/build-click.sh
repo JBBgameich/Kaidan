5bb2a22c21406601e7693acaf61c606ea70dc2fe
JBBgameich <jbbgameich@gmail.com>
2018-02-26 21:11:18 +0100

    Use Gloox from Plasma Mobile

--- utils/build-click.sh
+++ utils/build-click.sh
@@ -19,10 +19,10 @@ echo "Downloading Kirigami from KDE Neon"
 echo "*****************************************"
 
 for PKG in qml-module-org-kde-kirigami2 kirigami2-dev libkf5kirigami2-5; do
-	SUFFIX="5.43.0+p16.04+git20180221.0430-0_armhf"
-	wget https://archive.neon.kde.org/dev/unstable/pool/main/k/kirigami2/${PKG}_${SUFFIX}.deb -O /tmp/${PKG}_${SUFFIX}.deb
-	dpkg-deb -x /tmp/${PKG}_${SUFFIX}.deb $CLICK_TARGET_DIR
-	rm /tmp/${PKG}_${SUFFIX}.deb
+	VERSION="5.43.0+p16.04+git20180221.0430-0"
+	wget https://archive.neon.kde.org/dev/unstable/pool/main/k/kirigami2/${PKG}_${VERSION}_armhf.deb -O /tmp/${PKG}_${VERSION}_armhf.deb
+	dpkg-deb -x /tmp/${PKG}_${VERSION}_armhf.deb $CLICK_TARGET_DIR
+	rm /tmp/${PKG}_${VERSION}_armhf.deb
 done
 
 
@@ -31,28 +31,29 @@ echo "Downloading QtQuick Controls 2 from UBports"
 echo "*****************************************"
 
 for PKG in qml-module-qtquick-controls2 qtquickcontrols2-5-dev qml-module-qtquick-templates2 qml-module-qt-labs-platform libqt5quicktemplates2-5 libqt5quicktemplates2-5; do
-	SUFFIX="5.9.3-0ubports2_armhf"
-	wget https://repo.ubports.com/pool/xenial/main/q/qtquickcontrols2-opensource-src/${PKG}_${SUFFIX}.deb -O /tmp/${PKG}_${SUFFIX}.deb
-	dpkg-deb -x /tmp/${PKG}_${SUFFIX}.deb $CLICK_TARGET_DIR
-	rm /tmp/${PKG}_${SUFFIX}.deb
+	VERSION="5.9.3-0ubports2"
+	wget https://repo.ubports.com/pool/xenial/main/q/qtquickcontrols2-opensource-src/${PKG}_${VERSION}_armhf.deb -O /tmp/${PKG}_${VERSION}_armhf.deb
+	dpkg-deb -x /tmp/${PKG}_${VERSION}_armhf.deb $CLICK_TARGET_DIR
+	rm /tmp/${PKG}_${VERSION}_armhf.deb
 done
 
-
 echo "*****************************************"
-echo "Building Gloox"
+echo "Downloading Gloox from Plasma Mobile"
 echo "*****************************************"
 
-cd $KAIDAN_SOURCES/3rdparty/gloox
-./autogen.sh
-./configure --host=arm --prefix=/usr/
-DESTDIR=$CLICK_TARGET_DIR make install -j$(nproc)
-cd $KAIDAN_SOURCES
+for PKG in libgloox-dev libgloox17; do
+	VERSION="1.0.20-1+16.04+xenial+build1"
+	wget http://neon.plasma-mobile.org:8080/pool/main/g/gloox/${PKG}_${VERSION}_armhf.deb -O /tmp/${PKG}_${VERSION}_armhf.deb
+	dpkg-deb -x /tmp/${PKG}_${VERSION}_armhf.deb $CLICK_TARGET_DIR
+	rm /tmp/${PKG}_${VERSION}_armhf.deb
+done
+
 
 echo "*****************************************"
 echo "Building Kaidan"
 echo "*****************************************"
 
-! [ -d $KAIDAN_SOURCES/build ] && mkdir $KAIDAN_SOURCES/build; cd build
+! [ -d $KAIDAN_SOURCES/build ] && mkdir $KAIDAN_SOURCES/build
 cd $KAIDAN_SOURCES/build
 cmake .. -DUBUNTU_TOUCH=1 -DCMAKE_INSTALL_PREFIX=/usr/
 DESTDIR=$CLICK_TARGET_DIR make VERBOSE=1 install
@@ -61,6 +62,16 @@ cp $KAIDAN_SOURCES/misc/ubuntu-touch/manifest.json $CLICK_TARGET_DIR
 cp $KAIDAN_SOURCES/misc/ubuntu-touch/apparmor.json $CLICK_TARGET_DIR
 cp $KAIDAN_SOURCES/misc/ubuntu-touch/kaidan.desktop $CLICK_TARGET_DIR
 cp $KAIDAN_SOURCES/misc/kaidan.svg $CLICK_TARGET_DIR
+
+# Strip out documentation and includes
+rm -r \
+	$KAIDAN_SOURCES/bin/ubuntu-touch/usr/include \
+	$KAIDAN_SOURCES/bin/ubuntu-touch/usr/share/doc \
+	$KAIDAN_SOURCES/bin/ubuntu-touch/usr/lib/arm-linux-gnueabihf/cmake/ \
+	$KAIDAN_SOURCES/bin/ubuntu-touch/usr/lib/arm-linux-gnueabihf/pkgconfig/
+
+# # Move everything from usr/ to parent dir
+# mv $KAIDAN_SOURCES/bin/ubuntu-touch/usr/* $KAIDAN_SOURCES/bin/ubuntu-touch/
 
 cd $CLICK_TARGET_DIR
 
