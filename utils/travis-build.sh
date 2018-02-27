e4ac7061a317b5a880aac6e82f921ba2b52b3206
JBBgameich <jbbgameich@gmail.com>
2018-02-27 18:22:03 +0100

    travis x1

--- utils/travis-build.sh
+++ utils/travis-build.sh
@@ -9,9 +9,11 @@ echo "*****************************************"
 mkdir ../build; cd ../build/
 
 if [ $BUILD_SYSTEM == "cmake" ]; then
+	source /opt/qt*/bin/qt*-env.sh
 	cmake .. -GNinja -DI18N=1 -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
 	cmake --build .
 elif [ $BUILD_SYSTEM == "qmake" ]; then
+	source /opt/qt*/bin/qt*-env.sh
 	lrelease ../Kaidan.pro
 	qmake "QMAKE_CXX=ccache g++" ..
 	make -j$(nproc)
