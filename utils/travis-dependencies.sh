44080bb910ba16f69d9f403ce9d6de3f39d5921d
JBBgameich <jbbgameich@gmail.com>
2018-02-27 18:18:59 +0100

    Initial clickable travis integration

--- utils/travis-dependencies.sh
+++ utils/travis-dependencies.sh
@@ -12,22 +12,45 @@ sudo apt-get install software-properties-common dirmngr gnupg ca-certificates wg
 # Trusty backports
 sudo bash -c "echo deb http://archive.ubuntu.com/ubuntu trusty-backports main restricted universe multiverse >> /etc/apt/sources.list"
 
-# Qt 5.9 repository
-sudo add-apt-repository ppa:beineri/opt-qt593-trusty -y
+if ! [ -z $BUILD_SYSTEM ]; then
+	# Qt 5.9 repository
+	sudo add-apt-repository ppa:beineri/opt-qt593-trusty -y
+fi
 
+# Clickable repository
+if [ BUILD_VARIANT == "click" ]; then
+	sudo add-apt-repository ppa:bhdouglass/clickable
+fi
+
+# Refresh package lists
 sudo apt-get update
-sudo apt-get install cmake \
-            build-essential \
-            ninja-build \
-            zlib1g-dev \
-            qt59base \
-            qt59script \
-            qt59declarative \
-            qt59tools \
-            qt59x11extras \
-            qt59svg \
-            qt59quickcontrols2 \
-            -y -t trusty-backports
+
+if ! [ -z $BUILD_SYSTEM ]; then
+	sudo apt-get install cmake \
+		build-essential \
+		ninja-build \
+		zlib1g-dev \
+		qt59base \
+		qt59script \
+		qt59declarative \
+		qt59tools \
+		qt59x11extras \
+		qt59svg \
+		qt59quickcontrols2 \
+		-y -t trusty-backports
+
+	# Gloox
+	wget -c "https://github.com/JBBgameich/precompiled-kf5-linux/releases/download/KF5.40/libgloox17_1.0.20-1_amd64.deb"
+	wget -c "https://github.com/JBBgameich/precompiled-kf5-linux/releases/download/KF5.40/libgloox-dev_1.0.20-1_amd64.deb"
+	sudo dpkg -i libgloox*.deb
+	rm libgloox*.deb
+	sudo apt install -f -y
+fi
+
+if [ BUILD_VARIANT == "click" ]; then
+	sudo apt-get install clickable
+	clickable setup-docker
+fi
 
 # KF5 (only needed for cmake)
 if [ $BUILD_SYSTEM == "cmake" ]; then
@@ -35,11 +58,3 @@ if [ $BUILD_SYSTEM == "cmake" ]; then
 	tar xf kf5.40-gcc6-linux64-release.tar.xz
 	sudo cp -rf kf5-release/* /opt/qt*/
 fi
-
-# Gloox
-wget -c "https://github.com/JBBgameich/precompiled-kf5-linux/releases/download/KF5.40/libgloox17_1.0.20-1_amd64.deb"
-wget -c "https://github.com/JBBgameich/precompiled-kf5-linux/releases/download/KF5.40/libgloox-dev_1.0.20-1_amd64.deb"
-sudo dpkg -i libgloox*.deb
-rm libgloox*.deb
-sudo apt install -f -y
-
